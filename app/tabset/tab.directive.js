e6aa50598a3c372ad1d156d8fd61204f8dce07f9
Tomasz Bialecki <tomasz.bialecki@capgemini.com>
2014-10-24 14:16:27 +0200

    tab,pane,tabset direcitves

--- app/tabset/tab.directive.js
+++ app/tabset/tab.directive.js
@@ -1,11 +1,18 @@
 angular.module('tabset').directive('tab', function () {
     return{
         restrict: 'AE',
+        require: '^tabset',
         scope: {
             title: '@'
         },
         templateUrl: 'tabset/tab.html',
         transclude: true,
-        replace:true
+        replace: true,
+        link: function (scope, element, attrs, tabsetCntl) {
+            scope.tabState = {
+                title: scope.title
+            };
+            tabsetCntl.addTab(scope.tabState);
+        }
     }
 });
--- app/tabset/tabset.directive.js
+++ app/tabset/tabset.directive.js
@@ -3,6 +3,23 @@ angular.module('tabset').directive('tabset', function () {
         restrict: 'AE',
         scope: true,
         templateUrl: 'tabset/tabset.html',
-        replace:true
+        replace: true,
+        transclude: true,
+        controller: function ($scope) {
+            var ctrl = this,
+                tabs = ctrl.tabs = $scope.tabs = [];
+            ctrl.addTab = function (tab) {
+                tabs.push(tab);
+                if (tabs.length === 1) {
+                    tab.active = 1;
+                }
+            };
+            $scope.selectTab = function (index) {
+                angular.forEach(tabs, function (tab) {
+                    tab.active = false;
+                });
+                tabs[index].active = true;
+            };
+        }
     }
 });
