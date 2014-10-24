c74f7a273ff1b5bc425f1127f2554322d15b159a
Tomasz Bialecki <tomasz.bialecki@capgemini.com>
2014-10-24 13:52:50 +0200

    tab,pane,tabset direcitves

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module('dd', ['simple-directive'])
+angular.module('dd', ['simple-directive', 'tabset'])
     .controller('MainCntl', function ($scope) {
         $scope.message = "Hello World";
     });
