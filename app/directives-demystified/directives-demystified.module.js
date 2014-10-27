adcb76eda3de492374838b5a0ae0214c2ff2e670
Tomasz Bialecki <tomasz.bialecki@capgemini.com>
2014-10-27 13:10:11 +0100

    directive definition, scope, directive matching

--- app/directives-demystified/directives-demystified.module.js
+++ app/directives-demystified/directives-demystified.module.js
@@ -1,22 +1,23 @@
 var scopeToString = function ($scope) {
     return '\tscope=' + $scope.$id + ', parentScope=' + $scope.$parent.$id;
 };
-angular.module('directives-demystified', []).directive('dirA', function ($log) {
-    return {
-        restrict: 'A',
-        controller: function ($scope) {
-            $log.debug('Controller \tdir-a' + scopeToString($scope))
-        },
-        compile: function () {
-            $log.debug('Compile \tdir-a');
-            return {
-                pre: function ($scope) {
-                    $log.debug('Prelink \tdir-a');
-                },
-                post: function ($scope) {
-                    $log.debug('Postlink \tdir-a');
+angular.module('directives-demystified', [])
+    .directive('dirA', function ($log) {
+        return {
+            restrict: 'A',
+            controller: function ($scope) {
+                $log.debug('Controller \tdir-a' + scopeToString($scope))
+            },
+            compile: function () {
+                $log.debug('Compile \tdir-a');
+                return {
+                    pre: function ($scope) {
+                        $log.debug('Prelink \tdir-a');
+                    },
+                    post: function ($scope) {
+                        $log.debug('Postlink \tdir-a');
+                    }
                 }
             }
         }
-    }
-});
+    });
